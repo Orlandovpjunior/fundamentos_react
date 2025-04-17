d5ca7163d8fffaac526a63d25cf3848dd3103d72
orlandovpjunior <orlando.virginio.penha.junior@ccc.ufcg.edu.br>
2025-04-17 10:57:23 -0300

    refactor: termina sideBar e adiciona biblioteca de icones

--- site/src/components/SideBar.jsx
+++ site/src/components/SideBar.jsx
@@ -1,4 +1,5 @@
 import styles from './SideBar.module.css';
+import {PencilLine} from 'phosphor-react'
 
 export function SideBar(){
     return(
@@ -8,12 +9,17 @@ export function SideBar(){
                 src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             />
             <div className={styles.profile}>
+                <img 
+                    className={styles.avatar} 
+                    src="https://github.com/orlandovpjunior.png"
+                />
                 <strong>Orlando Junior</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar o seu perfil
                 </a>
             </footer>
